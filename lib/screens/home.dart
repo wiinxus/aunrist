0eea62f05ca0186150dae3b82c1d27ab8c4c1161
wiinxus <wiinxus@gmail.com>
2019-11-05 10:48:00 +0700

    Get JSON Success

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -26,16 +29,20 @@ class _HomeState extends State<Home> {
     );
   }
 
-Future<void> readQRcode()async{
-
-  try {
-  resultCode = await BarcodeScanner.scan();
-  print('resultCode = $resultCode');
-    
-    
-  } catch (e) {
+  Future<void> readQRcode() async {
+    try {
+      resultCode = await BarcodeScanner.scan();
+      print('resultCode = $resultCode');
+      getUserWhereResultCode();
+    } catch (e) {}
   }
-}
+  Future<void> getUserWhereResultCode() async {
+     String urlAPI = 'http://10.28.50.26/getUserWhereResultAun.php?isAdd=true&ResultCode=$resultCode';
+     Response response = await get(urlAPI);
+    //  print('response = $response');
+    var result = json.decode(response.body);
+    print('result ===========================================>>>>>>>>>>>>>>>>> $result');
+  }
 
   Widget showLogo() {
     return Container(
